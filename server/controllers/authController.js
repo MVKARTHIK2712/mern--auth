01519ee68c883e6a0b361547e943dd493555e553
MVKARTHIK2712 <karnamuraboina@gmail.com>
2025-09-07 23:39:58 +0530

    created express server+connected to mongodb+userschema+store user+generate token+add token to cookie

---
+++ server/controllers/authController.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+
+export const register = async(req, res) => {
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({succes:false,message:'All fields are required'});
+    }
+    try{
+        const existingUser=await userModel.findOne({email});
+        if(existingUser){
+            return res.status(400).json({succes:false,message:'User already exists'});
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new userModel({name, email, password:hashedPassword});
+        await user.save();
+
+        const token=jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn:'12d'});
+
+        res.cookie('token', token, {
+            httpOnly:true,
+            secure:process.env.NODE_ENV==='production',
+            sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+            maxAge:12*24*60*60*1000
+        })
+    }
+    catch(error){
+        res.json({message:error.message});
+    }
+}
